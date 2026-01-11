2af8add4b1e30ca93f88f1982bc2196702166f3c
FuzzyExpress <127012288+FuzzyExpress@users.noreply.github.com>
2026-01-11 03:47:54 -0600

    Shooting and Lead testing - Due for cleanup

---
+++ Globals.gd
@@ -0,0 +1,58 @@
+extends Node
+
+func lerp(a: float, b: float, t: float) -> float:
+	"""Linear interpolate on the scale given by a to b, using t as the point on that scale.
+	Examples
+	--------
+		50 == lerp(0, 100, 0.5)
+		4.2 == lerp(1, 5, 0.8)
+	"""
+	return (1 - t) * a + t * b
+
+
+func inv_lerp(a: float, b: float, v: float) -> float:
+	"""Inverse Linar Interpolation, get the fraction between a and b on which v resides.
+	Examples
+	--------
+		0.5 == inv_lerp(0, 100, 50)
+		0.8 == inv_lerp(1, 5, 4.2)
+	"""
+	return (v - a) / (b - a)
+
+
+func remap(i_min: float, i_max: float, o_min: float, o_max: float, v: float) -> float:
+	"""Remap values from one linear scale to another, a combination of lerp and inv_lerp.
+	i_min and i_max are the scale on which the original value resides,
+	o_min and o_max are the scale to which it should be mapped.
+	Examples
+	--------
+		45 == remap(0, 100, 40, 50, 50)
+		6.2 == remap(1, 5, 3, 7, 4.2)
+	"""
+	return lerp(o_min, o_max, inv_lerp(i_min, i_max, v))
+
+func get_intercept(shooter_pos:Vector3,
+					bullet_speed:float,
+					target_position:Vector3,
+					target_velocity:Vector3) -> Vector3:
+	var a:float = bullet_speed*bullet_speed - target_velocity.dot(target_velocity)
+	var b:float = 2*target_velocity.dot(target_position-shooter_pos)
+	var c:float = (target_position-shooter_pos).dot(target_position-shooter_pos)
+	# Protect against divide by zero and/or imaginary results
+	# which occur when bullet speed is slower than target speed
+	var time:float = 0.0
+	if bullet_speed > target_velocity.length():
+		time = (b+sqrt(b*b+4*a*c)) / (2*a)
+	return target_position+time*target_velocity
+
+func EaseIOCubic(x: float) -> float:
+	"""Cubic easing in/out - acceleration until halfway, then deceleration.
+	Examples
+	--------
+		0.5 == easeInOutCubic(0.5)
+		0.896 == easeInOutCubic(0.8)
+	"""
+	if x < 0.5:
+		return 4 * x * x * x
+	else:
+		return 1 - pow(-2 * x + 2, 3) / 2
---
+++ turret.gd
@@ -0,0 +1,53 @@
+extends Node3D
+
+@onready var Base	 = $Base
+@onready var Yaw	 = $Base/Yaw
+@onready var Pitch	 = $Base/Yaw/Pitch
+@onready var Head	 = $Base/Yaw/Pitch/Head
+@onready var Emitter = $Base/Yaw/Pitch/Head/Emitter
+
+@onready var Target	= $"../Bad"
+
+var CDown : int = 0
+var CoolingTime : int = 8
+
+
+var Projectile = preload("res://projectile_3d.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	
+	var t = Globals.get_intercept( Emitter.global_position, 20, Target.global_position, Target.velocity )
+	$"../Area3D".global_position = t
+	# Ratate base
+	Yaw.look_at(t, Vector3(0,1,0))
+	Yaw.rotation.x = 0
+	Yaw.rotation.z = 0
+	
+	# Rotate turret head
+	Pitch.look_at(t, Vector3(0,1,0))
+	Pitch.rotation.y = 0
+	Pitch.rotation.z = 0
+	
+	
+	
+	
+	if CDown == 0:
+		CDown = CoolingTime
+		
+		var projectile : Node3D = Projectile.instantiate()
+		$"..".add_child(projectile)
+		projectile.global_transform = Emitter.global_transform
+		
+	else:
+		CDown -= 1;
+		
+	Head.position.z = Globals.remap( 0, CoolingTime, 0, 0.289, CDown )
+	
+	#Z.look_at(Target.global_position, Vector3(0, 1, 0))
+	#Y.look_at(Target.global_position, Vector3(0, 1, 0))
